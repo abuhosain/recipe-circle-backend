235c591aabd8bf1225b6575a402bc097493f8b07
Abu Hosain <crypticmin925@gmail.com>
2024-10-11 04:32:14 +0600

    user and recipe create done for admin

--- src/app/modules/Admin/admin.serrvice.ts
+++ src/app/modules/Admin/admin.serrvice.ts
@@ -3,6 +3,15 @@ import AppError from '../../errors/AppError'
 import { User } from '../Auth/auth.model'
 import { Recipe } from '../Recipes/recipes.model'
 
+const getAllUsrFromDb = async () => {
+  const result = await User.find({ isDeleted : false});
+  return result
+}
+const getAllRecipeFromDb = async () => {
+  const result = await Recipe.find({ isDeleted : false});
+  return result
+}
+
 const blockUser = async (id: string) => {
   const user = await User.findById(id)
   if (!user) {
@@ -16,7 +25,7 @@ const blockUser = async (id: string) => {
   return result
 }
 
-const unBlockUser = async (id: string) => {
+const unBlockUsers = async (id: string) => {
   const user = await User.findById(id)
   if (!user) {
     throw new AppError(httpStatus.NOT_FOUND, 'User not found')
@@ -30,10 +39,15 @@ const unBlockUser = async (id: string) => {
 }
 
 const deleteRecipesFromDb = async (id : string) => {
-  const result = await Recipe.findByIdAndDelete(id);
+  const result = await Recipe.findByIdAndUpdate(id, {isDeleted : true}, {new : true});
   return result;
 }
 
+
+const deleteUserFromDb = async (id : string) => {
+  const result = await User.findByIdAndUpdate(id, {isDeleted : true}, {new : true});
+  return result;
+}
 
 
 const publishRecipe = async (id: string) => {
@@ -64,8 +78,11 @@ const unPublishRecipe = async (id: string) => {
 
 export const AdminServices = {
   blockUser,
-  unBlockUser,
+  unBlockUsers,
   publishRecipe,
   unPublishRecipe,
-  deleteRecipesFromDb
+  deleteRecipesFromDb,
+  getAllUsrFromDb,
+  deleteUserFromDb,
+  getAllRecipeFromDb
 }
